42c4a2bc4a27f2cfb80d454a260f532cf7ced276
Deep Pande <deeppande2117@gmail.com>
2023-07-30 16:26:17 +0530

    Bar and Pie Chart Added

--- app.py
+++ app.py
@@ -74,20 +74,20 @@ else:
     filtered_df = df3[df3["Region"].isin(region) & df3["State"].isin(state) & df3["City"].isin(city)]
 
     
-# category_df = filtered_df.groupby(by = ["Category"], as_index = False)["Sales"].sum()
+category_df = filtered_df.groupby(by = ["Category"], as_index = False)["Sales"].sum()
 
-# with col1:
-#     st.subheader("category wise sales")
-#     fig = px.bar(category_df, x ="Category", y= "Sales", text=['${:,.2f}'.format(x) for x in category_df["Sales"]],
-#                 template="seaborn" )
-#     st.plotly_chart(fig, use_container_width=True, height=200)
+with col1:
+    st.subheader("category wise sales")
+    fig = px.bar(category_df, x ="Category", y= "Sales", text=['${:,.2f}'.format(x) for x in category_df["Sales"]],
+                template="seaborn" )
+    st.plotly_chart(fig, use_container_width=True, height=200)
 
 
-# with col2:
-#     st.subheader("Region wise sales")
-#     fig = px.pie(filtered_df, values="Sales", names = "Region", hole = 0.5)
-#     fig.update_traces (text= filtered_df["Region"], textposition= "outside") 
-#     st.plotly_chart(fig, use_container_width=True) 
+with col2:
+    st.subheader("Region wise sales")
+    fig = px.pie(filtered_df, values="Sales", names = "Region", hole = 0.5)
+    fig.update_traces (text= filtered_df["Region"], textposition= "outside") 
+    st.plotly_chart(fig, use_container_width=True) 
 
 
 
