00657b1d56bf156bc21185cedd37bced665e8632
Deep Pande <deeppande2117@gmail.com>
2023-07-30 16:24:34 +0530

    Date and region added

--- app.py
+++ app.py
@@ -22,56 +22,56 @@ else:
     df =pd.read_csv("Superstore.csv", encoding="ISO-8859-1")
 
 
-# col1 , col2 = st.columns((2)) 
-# df["Order Date"] = pd.to_datetime(df["Order Date"])
+col1 , col2 = st.columns((2)) 
+df["Order Date"] = pd.to_datetime(df["Order Date"])
 
-# startDate = pd.to_datetime(df["Order Date"]).min()
-# endDate = pd.to_datetime(df["Order Date"]).max()
+startDate = pd.to_datetime(df["Order Date"]).min()
+endDate = pd.to_datetime(df["Order Date"]).max()
 
-# with col1:
-#     date1 = pd.to_datetime(st.date_input("Start Date", startDate))
+with col1:
+    date1 = pd.to_datetime(st.date_input("Start Date", startDate))
 
-# with col2:
-#     date2 = pd.to_datetime(st.date_input("End Date", endDate))
-
-
-# df = df[(df["Order Date"] >= date1) & (df["Order Date"] <= date2)].copy()
-
-# st.sidebar.header("Choose your filter: ")
-# region = st.sidebar.multiselect("Pick your Region", df["Region"].unique())
-# if not region:
-#     df2 = df.copy()
-# else:
-#     df2 = df[df["Region"].isin(region)]
+with col2:
+    date2 = pd.to_datetime(st.date_input("End Date", endDate))
 
 
-# state = st.sidebar.multiselect("Pick the state", df2["State"].unique())
-# if not state:
-#     df3 = df2.copy()
-# else:
-#     df3 = df2[df2["State"].isin(state)]    
+df = df[(df["Order Date"] >= date1) & (df["Order Date"] <= date2)].copy()
 
-# city = st.sidebar.multiselect("Pick the city", df3["City"].unique())
+st.sidebar.header("Choose your filter: ")
+region = st.sidebar.multiselect("Pick your Region", df["Region"].unique())
+if not region:
+    df2 = df.copy()
+else:
+    df2 = df[df["Region"].isin(region)]
+
+
+state = st.sidebar.multiselect("Pick the state", df2["State"].unique())
+if not state:
+    df3 = df2.copy()
+else:
+    df3 = df2[df2["State"].isin(state)]    
+
+city = st.sidebar.multiselect("Pick the city", df3["City"].unique())
 
 
 
 
-# if not region and not state and not city:
-#     filtered_df = df
-# elif not state and not city:
-#     filtered_df = df[df["Region"].isin(region)]
-# elif not region and not city:
-#     filtered_df = df[df["State"].isin(state)] 
-# elif state and city:
-#     filtered_df= df3[df["State"].isin(state)& df3["City"].isin(city)] 
-# elif region and city:
-#     filtered_df = df3[df["Region"].isin(region) & df3["City"].isin(city) ]
-# elif region and state:
-#     filtered_df = df3[df["Region"].isin(region) & df3["State"].isin(state)]
-# elif city:
-#     filtered_df = df3[df3["City"].isin(city)]
-# else:
-#     filtered_df = df3[df3["Region"].isin(region) & df3["State"].isin(state) & df3["City"].isin(city)]
+if not region and not state and not city:
+    filtered_df = df
+elif not state and not city:
+    filtered_df = df[df["Region"].isin(region)]
+elif not region and not city:
+    filtered_df = df[df["State"].isin(state)] 
+elif state and city:
+    filtered_df= df3[df["State"].isin(state)& df3["City"].isin(city)] 
+elif region and city:
+    filtered_df = df3[df["Region"].isin(region) & df3["City"].isin(city) ]
+elif region and state:
+    filtered_df = df3[df["Region"].isin(region) & df3["State"].isin(state)]
+elif city:
+    filtered_df = df3[df3["City"].isin(city)]
+else:
+    filtered_df = df3[df3["Region"].isin(region) & df3["State"].isin(state) & df3["City"].isin(city)]
 
     
 # category_df = filtered_df.groupby(by = ["Category"], as_index = False)["Sales"].sum()
